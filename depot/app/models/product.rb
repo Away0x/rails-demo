a8841f8351e94c1a11c943717f26564cc6040272
Away0x <wutong0910@foxmail.com>
2020-07-19 14:01:15 +0800

    depot test

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # title 唯一
+  validates :title, uniqueness: true
+  # 验证 image url 是否有效
+  # allow_blank 防止 url 为空时产生多条错误信息
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
 end
