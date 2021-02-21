4f09885a3eaad742749dfa01ea63d8c4bc0d61dc
WuTong <tong.wu@rocketg.com>
2021-02-21 20:12:20 +0800

    update weibo app makefile

--- weibo/app/models/user.rb
+++ weibo/app/models/user.rb
@@ -131,12 +131,12 @@ class User < ApplicationRecord
     #                 user_id: id)
 
     # 使用子查询获取微博动态流 (用户多时效率比上面的 sql 高)
-    # following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-    # Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
 
     # 使用 join 方法输出动态流
-    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
-    Micropost.joins(user: :followers).where(part_of_feed, { id: id })
+    # part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    # Micropost.joins(user: :followers).where(part_of_feed, { id: id })
   end
 
   # 关注另一个用户
